63095b70dadc45499de85d57c4314b2ed1848624
mahg0509 <mahg0509@outlook.com>
2024-03-17 20:29:05 +0530

    Add new vnet2

--- variables.tf
+++ variables.tf
@@ -4,3 +4,4 @@ variable "env" {}
 variable "batch" {}
 
 variable "vnet1_cidr" {}
+variable "vnet2_cidr" {}
--- vnet.tf
+++ vnet.tf
@@ -9,3 +9,16 @@ resource "azurerm_virtual_network" "demo_vnet1" {
     batch = var.batch
   }
 }
+
+
+resource "azurerm_virtual_network" "demo_vnet2" {
+  name                = "${azurerm_resource_group.rg1.name}-vnet2"
+  address_space       = [ var.vnet2_cidr ]
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+
+  tags = {
+    env   = var.env
+    batch = var.batch
+  }
+}
