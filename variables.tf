16e1aadd6eca43d9e003cd3e7535aa16d6f6676c
mahg0509 <mahg0509@outlook.com>
2024-03-17 21:27:50 +0530

    added three subnets in vnet1

--- variables.tf
+++ variables.tf
@@ -5,3 +5,6 @@ variable "batch" {}
 
 variable "vnet1_cidr" {}
 variable "vnet2_cidr" {}
+variable "subnet1_cidr" {}
+variable "subnet2_cidr" {}
+variable "subnet3_cidr" {}
--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "demo_vnet1" {
   name                = "${azurerm_resource_group.rg1.name}-vnet1"
-  address_space       = [ var.vnet1_cidr ]
+  address_space       = [var.vnet1_cidr]
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
 
@@ -13,7 +13,7 @@ resource "azurerm_virtual_network" "demo_vnet1" {
 
 resource "azurerm_virtual_network" "demo_vnet2" {
   name                = "${azurerm_resource_group.rg1.name}-vnet2"
-  address_space       = [ var.vnet2_cidr ]
+  address_space       = [var.vnet2_cidr]
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
 
@@ -21,4 +21,26 @@ resource "azurerm_virtual_network" "demo_vnet2" {
     env   = var.env
     batch = var.batch
   }
+}
+
+
+resource "azurerm_subnet" "demo_subnet1" {
+  name                 = "${azurerm_virtual_network.demo_vnet1.name}-subnet1"
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.demo_vnet1.name
+  address_prefixes     = [var.subnet1_cidr]
+}
+
+resource "azurerm_subnet" "demo_subnet2" {
+  name                 = "${azurerm_virtual_network.demo_vnet1.name}-subnet2"
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.demo_vnet1.name
+  address_prefixes     = [var.subnet2_cidr]
+}
+
+resource "azurerm_subnet" "demo_subnet3" {
+  name                 = "${azurerm_virtual_network.demo_vnet1.name}-subnet3"
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.demo_vnet1.name
+  address_prefixes     = [var.subnet3_cidr]
 }
