f2a57abb681aa6b7388b3931881b039f632a707d
mahg0509 <mahg0509@outlook.com>
2024-03-18 15:10:27 +0530

    added storage using count

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "testrg"
+    resource_group_name  = "terraformrg"
     storage_account_name = "devops32backendtfstate"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
--- storage_account.tf
+++ storage_account.tf
@@ -1,11 +1,13 @@
 resource "azurerm_storage_account" "demostg" {
-  name                     = "devops32backendtfstate"
+  count                    = 3
+  name                     = "demostgdevops${count.index}"
   resource_group_name      = azurerm_resource_group.rg1.name
   location                 = azurerm_resource_group.rg1.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
   tags = {
-    env = var.env
+    env   = var.env
+    batch = var.batch
   }
 }
